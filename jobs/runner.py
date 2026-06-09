fb85f0b44e13ff795a791e5beeabb3a637222473
Kada Haála <kada626@gmail.com>
2026-06-09 22:59:00 +0200

    update runner logic

--- jobs/runner.py
+++ jobs/runner.py
@@ -1,16 +1,21 @@
-
 from app import app
 from jobs.update_matches_job import run_job
 import time
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 if __name__ == "__main__":
     with app.app_context():
-        while True:
+
+        start = datetime.now(timezone.utc)
+        duration = timedelta(minutes=24*60)
+
+        while datetime.now(timezone.utc) - start < duration:
             try:
                 now = datetime.now(timezone.utc)
                 print(f"[{now.isoformat()}] tick")
+
                 run_job()
+
             except Exception as e:
                 print("Error:", e)
 
