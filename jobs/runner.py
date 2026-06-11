d05dce18f9845a78bfc883e936f9a8a88443f2a1
Kada Haála <kada626@gmail.com>
2026-06-11 12:33:48 +0200

    reduce job runner frequency to 30 mins (so it doesn't time out on pythonanywhere)

--- jobs/runner.py
+++ jobs/runner.py
@@ -32,4 +32,4 @@ if __name__ == "__main__":
             except Exception as e:
                 logger.error(f"Error: {e}")
 
-            time.sleep(300)  # 5 minutes
+            time.sleep(1800)  # 5 minutes
